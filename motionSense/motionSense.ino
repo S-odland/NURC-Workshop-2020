fb2f056ae414eca105fc869f76f9cffc798b9fe7
S-odland <49540258+S-odland@users.noreply.github.com>
2020-10-03 12:04:51 -0500

    led notification for 'intruder'

--- motionSense/motionSense.ino
+++ motionSense/motionSense.ino
@@ -4,11 +4,11 @@
 BluetoothSerial SerialBT; //defining an instance of BluetoothSerial to be SerialBT
 
 const int motionSensePin = A4; // phototransistor on pin A4 of the Huzzah
-//const int led1 = 27;
+const int onBoardLED = 13; // built in LED on the esp32 HUZZAH
  
 void setup()
 {
-  //pinMode(led1, OUTPUT); // make red led an ouput indicator for accelerometer movement
+  pinMode(onBoardLED, OUTPUT); // make red led an ouput indicator for accelerometer movement
   pinMode(motionSensePin, INPUT);
   Serial.begin(9600); // Serial baud rate of 9600
   SerialBT.begin("MotionSense"); // begins bluetooth for esp named MotionSense
@@ -20,6 +20,7 @@ void loop()
   motionSense = analogRead(motionSensePin);
 
   if (motionSense < 50){ // threshold value to detect an "intruder"
+    digitalWrite(onBoardLED, HIGH); // light goes on if "intruder" is detected
     SerialBT.println("INTRUDER");
   }
   delay(100);
