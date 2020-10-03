1f4aeac10d85b0ee8cdaf40810df74e046f955c2
S-odland <49540258+S-odland@users.noreply.github.com>
2020-10-03 15:48:30 -0500

    added a non BT communication for continuity of the workshop

---
+++ wiredMotionSense/wiredMotionSense.ino
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+const int motionSensePin = A4; // phototransistor on pin A4 of the Huzzah
+const int onBoardLED = 13; // built in LED on the esp32 HUZZAH
+ 
+void setup()
+{
+  pinMode(onBoardLED, OUTPUT); // make red led an ouput indicator for accelerometer movement
+  pinMode(motionSensePin, INPUT);
+  Serial.begin(9600); // Serial baud rate of 9600
+
+}
+
+void loop()
+{
+  int motionSense;
+  motionSense = analogRead(motionSensePin);
+
+  if (motionSense < 50){ // threshold value to detect an "intruder"
+    digitalWrite(onBoardLED, HIGH); // light goes on if "intruder" is detected
+    Serial.println("INTRUDER");
+  }
+  delay(100);
+}
