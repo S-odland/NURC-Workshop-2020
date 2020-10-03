3c03e91d31aedf988feb55a3cfe50bd2907661ef
S-odland <49540258+S-odland@users.noreply.github.com>
2020-10-03 00:23:22 -0500

    commenting code

--- motionSense/motionSense.ino
+++ motionSense/motionSense.ino
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "BluetoothSerial.h"
 
-BluetoothSerial SerialBT;
+BluetoothSerial SerialBT; //defining an instance of BluetoothSerial to be SerialBT
 
-const int motionSensePin = A4;
+const int motionSensePin = A4; // phototransistor on pin A4 of the Huzzah
 //const int led1 = 27;
  
 void setup()
@@ -11,7 +11,7 @@ void setup()
   //pinMode(led1, OUTPUT); // make red led an ouput indicator for accelerometer movement
   pinMode(motionSensePin, INPUT);
   Serial.begin(9600); // Serial baud rate of 9600
-  SerialBT.begin("MotionSense");
+  SerialBT.begin("MotionSense"); // begins bluetooth for esp named MotionSense
 }
 
 void loop()
@@ -19,8 +19,8 @@ void loop()
   int motionSense;
   motionSense = analogRead(motionSensePin);
 
-  if (motionSense < 50){
+  if (motionSense < 50){ // threshold value to detect an "intruder"
     SerialBT.println("INTRUDER");
   }
-  delay(50);
+  delay(100);
 }
